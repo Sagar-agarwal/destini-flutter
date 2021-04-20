dde4e4d2309565fa84a80ec5cef9c42e7612ed91
agarwal-sagar <agarwal.sagar@outlook.com>
2021-04-20 12:46:18 +0530

    step 18 & 19 complete

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -47,7 +47,6 @@ class StoryBrain {
     return _storyData.first.choice2;
   }
 
-//TODO: Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber which will be the choice number (int) made by the user.
   void nextStory(int choiceNumber) {}
 }
 
